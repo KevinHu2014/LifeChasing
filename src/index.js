0bafda1b9b370a47016f10f1bcbf7c9898a1db20
KevinHu2014 <kevinhu@weco.net>
2018-03-05 22:15:05 +0800

    更換按鈕顏色 預設的primary是使用indigo（#3f51b5）而非官網上看到的blue (#2196f3)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import blue from 'material-ui/colors/blue';
 import './index.css';
 import Main from './App';
 // import Main from './components/Map';
 import registerServiceWorker from './registerServiceWorker';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
 const App = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider theme={theme}>
     <Main />
   </MuiThemeProvider>
 );
