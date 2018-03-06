f0c0e742846260421546db7e0480a45e3a46e1d1
KevinHu2014 <kevinhu@weco.net>
2018-03-06 21:40:25 +0800

    將主題色改用Color tool 調出來的配色
    https://material.io/color/#!/?view.left=0&view.right=0&secondary.color=1976D2&primary.color=4CAF50

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import blue from 'material-ui/colors/blue';
 import './index.css';
 import Main from './App';
 // import Main from './components/Map';
@@ -10,7 +9,18 @@ import registerServiceWorker from './registerServiceWorker';
 
 const theme = createMuiTheme({
   palette: {
-    primary: blue,
+    primary: {
+      light: '#80e27e',
+      main: '#4caf50',
+      dark: '#087f23',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#63a4ff',
+      main: '#1976d2',
+      dark: '#004ba0',
+      contrastText: '#fff',
+    },
   },
 });
 
