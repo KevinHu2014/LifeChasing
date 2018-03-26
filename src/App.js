b9eed2db971c997fab3456d375423a0415a56c8e
KevinHu2014 <kevinhu@weco.net>
2018-03-26 22:28:42 +0800

    調整檔案結構

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react';
-// import ThreeButtonSelectionPage from './components/common/ThreeButtonSelectionPage';
-// import GameSegment from './components/common/GameSegment';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { createLogger } from 'redux-logger';
+import './index.css';
+import Main from './containers/Map';
 import GameRecord from './components/common/GameRecord';
+import reducers from './reducers';
+
+const logger = createLogger();
+const store = createStore(
+  reducers,
+  applyMiddleware(ReduxPromise, logger),
+);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#80e27e',
+      main: '#4caf50',
+      dark: '#087f23',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#63a4ff',
+      main: '#1976d2',
+      dark: '#004ba0',
+      contrastText: '#fff',
+    },
+  },
+});
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      test: 'App',
-    };
-  }
-
-  render() {
-    return (
-      <div className={this.state.test}>
-        {/* <ThreeButtonSelectionPage first="捷運輔大站" second="聖言樓" third="輔大側門" /> */}
-        <GameRecord />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/GameRecord" component={GameRecord} />
+            <Route path="/" component={Main} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </MuiThemeProvider>
+  </Provider>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,56 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import { createLogger } from 'redux-logger';
 import './index.css';
-// import Main from './App';
-import Main from './containers/Map';
-import GameRecord from './components/common/GameRecord';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducers from './reducers';
-
-const logger = createLogger();
-const store = createStore(
-  reducers,
-  applyMiddleware(ReduxPromise, logger),
-);
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#80e27e',
-      main: '#4caf50',
-      dark: '#087f23',
-      contrastText: '#000',
-    },
-    secondary: {
-      light: '#63a4ff',
-      main: '#1976d2',
-      dark: '#004ba0',
-      contrastText: '#fff',
-    },
-  },
-});
-
-const App = () => (
-  <Provider store={store}>
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/GameRecord" component={GameRecord} />
-            <Route path="/" component={Main} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </MuiThemeProvider>
-  </Provider>
-);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
