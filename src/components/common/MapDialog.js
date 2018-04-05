bc56a7cf334f22561e95219f3bafe83375c19f7f
KevinHu2014 <kevinhu@weco.net>
2018-04-05 10:35:05 +0800

    Make Dialog responsive

--- src/components/common/MapDialog.js
+++ src/components/common/MapDialog.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import List, { ListItem, ListItemText } from 'material-ui/List';
-import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  withMobileDialog,
+} from 'material-ui/Dialog';
 
+
+// eslint-disable-next-line prefer-stateless-function
 class MapDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+    };
+  }
+  handleClose() {
+    this.setState({ open: false });
+  }
+
   render() {
     return (
-      <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title">
-        <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
-        <div>
-          <List>
-            <ListItem button onClick={() => { console.log('clicked!'); }}>
-              <ListItemText primary="add account" />
-            </ListItem>
-          </List>
-        </div>
+      <Dialog open={this.state.open}>
+        <DialogTitle>Set backup account</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => { this.handleClose(); }} color="primary">
+              Disagree
+          </Button>
+          <Button onClick={() => { this.handleClose(); }} color="primary" autoFocus>
+              Agree
+          </Button>
+        </DialogActions>
       </Dialog>
     );
   }
 }
 
-export default MapDialog;
+export default withMobileDialog()(MapDialog);
