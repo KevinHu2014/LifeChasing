6ac6a57a76447b74108568672b0885c6f5155d79
KevinHu2014 <kevinhu@weco.net>
2018-05-14 08:54:25 +0800

    connect GameRecord to Firebase

--- src/components/presentational/GameRecord.js
+++ src/components/presentational/GameRecord.js
@@ -3,6 +3,9 @@ import List, { ListItem, ListItemText } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import Divider from 'material-ui/Divider';
 import { withRouter } from 'react-router-dom';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 import { BackAppBar } from '../common';
 
@@ -10,6 +13,46 @@ class GameRecord extends Component {
   constructor(props) {
     super(props);
     this.state = { };
+  }
+  componentDidMount() {
+    if (!isLoaded(this.props.game)) {
+      console.log(this.props.game);
+    }
+    if (isEmpty(this.props.game)) {
+      console.log('empty');
+    }
+  }
+  renderListItem() {
+    if (isLoaded(this.props.game)) {
+      return (
+        <List>
+          {
+            Object.values(this.props.game).map((item) => {
+              console.log(item);
+              const date = new Date();
+              date.setTime(item.startTime);
+              // millisecond -> minute
+              const costMinute = Math.round(item.expectTimeCost / 1000 / 60);
+              return (
+                <div key={item.startTime}>
+                  <ListItem>
+                    <Avatar>
+                      {item.mode[0]}
+                    </Avatar>
+                    <ListItemText
+                      primary={`${costMinute} Min`}
+                      secondary={date.toDateString()}
+                    />
+                  </ListItem>
+                  <Divider />
+                </div>
+              );
+            })
+          }
+        </List>
+      );
+    }
+    return (<div />);
   }
   render() {
     return (
@@ -24,29 +67,9 @@ class GameRecord extends Component {
           }}
         />
         <div className="grid">
-          <List>
-            <ListItem>
-              <Avatar>
-                F
-              </Avatar>
-              <ListItemText primary="15 min" secondary="Jan 9, 2014" />
-            </ListItem>
-            <Divider />
-            <ListItem>
-              <Avatar>
-                S
-              </Avatar>
-              <ListItemText primary="10 min" secondary="Jan 7, 2014" />
-            </ListItem>
-            <Divider />
-            <ListItem>
-              <Avatar>
-                M
-              </Avatar>
-              <ListItemText primary="15 min" secondary="July 20, 2014" />
-            </ListItem>
-            <Divider />
-          </List>
+          {
+            this.renderListItem()
+          }
         </div>
       </div>
     );
@@ -55,6 +78,29 @@ class GameRecord extends Component {
 
 GameRecord.propTypes = {
   history: PropTypes.shape().isRequired,
+  game: PropTypes.shape().isRequired,
 };
 
-export default withRouter(GameRecord);
+function mapStateToProps(state) {
+  return {
+    firebaseAuth: state.firebase.auth,
+    game: state.firebase.data.game,
+  };
+}
+
+const gameRecord = compose(
+  firebaseConnect((props, store) => {
+    const { firebase: { auth } } = store.getState();
+    // be careful, listeners are not re-attached when auth state changes unless props change
+    return [{
+      path: '/game',
+      queryParams: [
+        'orderByChild=userUid',
+        `equalTo=${auth.uid || ''}`,
+      ],
+    }];
+  }),
+  connect(mapStateToProps),
+)(GameRecord);
+
+export default withRouter(gameRecord);
