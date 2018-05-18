076b127117fb54237bdecaf37bf8970eb206a97b
KevinHu2014 <kevinhu@weco.net>
2018-05-18 16:17:31 +0800

    create actions and reducer for weather

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { firebaseReducer } from 'react-redux-firebase';
 
 import beanMap from './reducer_beanMap';
 import auth from './reducer_auth';
+import weather from './reducer_weather';
 
 const rootReducer = combineReducers({
   beanMap,
   firebase: firebaseReducer,
   auth,
+  weather,
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,13 @@
+// import {  } from '../actions/type';
+
+const INITAL_STATE = {
+};
+
+const weather = (state = INITAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default weather;
